70ad855d33bd516113a6778b5c0fef0b281c0914
Arslon004 <arslonbozorov2109@gmail.com>
2024-07-06 00:08:13 +0300

    changed navbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import menu from "../../assets/images/menu.svg";
+import close from "../../assets/images/close.svg";
 
 import "./Header.css";
+
 const Header = () => {
+
+  const [isMenuOpen,setIsMenuOpen]=useState(false);
+
+  const toggleMenu=()=>{
+    setIsMenuOpen(!isMenuOpen)
+  }
   return (
-    <header className='py-3 bg-secondary'>
+    <header className='py-4 bg-secondary'>
       <nav className="container">
-        <ul className="nav ">
+        <ul className="nav navbar--box">
           <li className="nav-item">
             <NavLink className="nav-link text-white" to="/home">Home</NavLink>
           </li>
@@ -14,12 +23,31 @@ const Header = () => {
             <NavLink className="nav-link text-white" to="/transaction">Transaction</NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link text-white" to="/debts">Debts</NavLink>
+            <NavLink className="nav-link text-white" to="/debts" >Debts</NavLink>
           </li>
         </ul>
+        <button className='hamburger' onClick={toggleMenu} >
+          <img src={menu} alt="menu" />
+        </button>
+      </nav>
+      <nav className={`nav navbar__responsive ${isMenuOpen ? "open" : ""}`}>
+        <ul className="res-nav-list">
+          <li className="nav__item--responsive">
+            <NavLink className="nav-link--responsive text-info" to="/home" onClick={toggleMenu}>Home</NavLink>
+          </li>
+          <li className="nav__item--responsive">
+            <NavLink className="nav-link--responsive text-info" to="/transaction" onClick={toggleMenu}>Transaction</NavLink>
+          </li>
+          <li className="nav__item--responsive">
+            <NavLink className="nav-link--responsive text-info" to="/debts" onClick={toggleMenu}>Debts</NavLink>
+          </li>
+        </ul>
+        <button onClick={toggleMenu}  className='close__btn' >
+          <img src={close} alt="close" />
+        </button>
       </nav>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
