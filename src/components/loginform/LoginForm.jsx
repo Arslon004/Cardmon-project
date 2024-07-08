c8aafcc072d2c96dde37cb379cbaf029ccf1f4c0
Arslon004 <arslonbozorov2109@gmail.com>
2024-07-08 12:31:58 +0300

    changed loginpage

--- src/components/loginform/LoginForm.jsx
+++ src/components/loginform/LoginForm.jsx
@@ -5,50 +5,52 @@ import { toast } from 'react-toastify';
 import "./LoginForm.css";
 
 const LoginForm = () => {
-  let [detail, setDetail] = useState({ username: "", password: "" });
-
   const navigate = useNavigate();
 
-  const login = (e) => {
-    e.preventDefault();
-    const storedUser = JSON.parse(localStorage.getItem('username'));
-    const storedPassword = JSON.parse(localStorage.getItem('password'));
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    if (detail.username === storedUser && detail.password === storedPassword) {
-      navigate("/home");
-    } else {
-      toast.error('Invalid username or password');
-    }
-  };
+  const storedUsername = JSON.parse(localStorage.getItem('username')) || null;
+  const storedPassword = JSON.parse(localStorage.getItem('password')) || null;
 
   const handleUsername = (e) => {
-    setDetail({ ...detail, username: e.target.value });
-  };
+    setUsername(e.target.value);
+  }
 
   const handlePassword = (e) => {
-    setDetail({ ...detail, password: e.target.value });
-  };
+    setPassword(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (username === storedUsername && password === storedPassword) {
+      toast.success("Login success");
+      navigate("/home");
+    } else {
+      toast.error("Invalid username or password");
+    }
+  }
 
   const handleRegister = (e) => {
     e.preventDefault();
-    localStorage.setItem('username', JSON.stringify(detail.username));
-    localStorage.setItem('password', JSON.stringify(detail.password));
-    toast.success('User registered successfully');
-  };
+    localStorage.setItem('username', JSON.stringify(username));
+    localStorage.setItem('password', JSON.stringify(password));
+    toast.success("User registered successfully");
+  }
 
   return (
     <div className="login__page">
       <div className='wrapper'>
-        <form onSubmit={login}>
+        <form onSubmit={handleSubmit}>
           <h1>Login</h1>
 
           <div className="input-box">
-            <input onChange={handleUsername} type="text" required placeholder='Username' value={detail.username} />
+            <input onChange={handleUsername} value={username} type="text" required placeholder='Username' />
             <FaUserAlt className='icon' />
           </div>
 
           <div className="input-box">
-            <input onChange={handlePassword} type="password" required placeholder='Password' value={detail.password} />
+            <input onChange={handlePassword} type="password" required placeholder='Password' value={password} />
             <FaLock className='icon' />
           </div>
 
@@ -60,7 +62,7 @@ const LoginForm = () => {
           <button type='submit'>Login</button>
 
           <div className="register-link">
-            <p>Don't have an account? <button onClick={handleRegister}>Register</button></p>
+            <p>Don't have an account? <button onClick={handleRegister} type='button'>Register</button></p>
           </div>
         </form>
       </div>
